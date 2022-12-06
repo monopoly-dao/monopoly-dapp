7c5cde265d1c9fb46b9a83f6ed1d685dda4cef46
Justin Irabor <justin.irabor@gmail.com>
2022-12-06 23:59:04 +0100

    Wallet wiring

--- dapp/components/Header/index.tsx
+++ dapp/components/Header/index.tsx
@@ -3,24 +3,48 @@ import Logo from '../Logo';
 import Button from '../Button';
 import { useRouter } from 'next/router';
 import {useEffect, useState} from 'react'
+import * as nearAPI from 'near-api-js';
+import { ConnectConfig } from 'near-api-js';
 
-export default function Header({walletConnection}) {
-    const router = useRouter();
-    console.log('WHHHHHH', walletConnection)
-    const [connection, setConnection] = useState<any>(null);
+
+export default function Header() {
+    
+    const { keyStores, connect, WalletConnection } = nearAPI;
+    const myKeyStore = new keyStores.BrowserLocalStorageKeyStore();
+    const connectionConfig: ConnectConfig = {
+        networkId: "testnet",
+        keyStore: myKeyStore,
+        nodeUrl: "https://rpc.testnet.near.org",
+        walletUrl: "https://wallet.testnet.near.org",
+        helperUrl: "https://helper.testnet.near.org",
+        headers: {}
+  };
+
+    const nearWalletConnection = async() =>  await connect(connectionConfig)
+        .then(nearConnection => {
+        const walletConnection = new WalletConnection(nearConnection, null);
+        return walletConnection
+  });
+
+    const [isWalletSignedIn, setIsWalletSignedIn] = useState<boolean>(false);
     
     const SignInToNear = async () => {
-        walletConnection.requestSignIn(
+        const conn = await nearWalletConnection()
+        conn.requestSignIn(
             "monopolydao.testnet",
             "MonopolyDAO"
           );
     }
 
-    const isWalletSignedIn = () => { 
-
-        // let status = walletConnection?.isWalletSignedIn();
-        // console.log('sytatus', status);
+    const isWalletActive = async () => { 
+        const conn = await nearWalletConnection()
+        conn.isSignedIn() ? setIsWalletSignedIn(true) : setIsWalletSignedIn(false);
     }
+
+    useEffect(() => {
+        isWalletActive();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
   return (
     <div>
@@ -34,7 +58,7 @@ export default function Header({walletConnection}) {
                 <Button
                     type="contained"
                     handleClick={SignInToNear}
-                    >Connect Wallet</Button>
+                    >{is}</Button>
             </div>
         </div>
     </div>
--- dapp/pages/index.tsx
+++ dapp/pages/index.tsx
@@ -1,8 +1,6 @@
 import WithFirebaseAuth from '../utils/initAuth';
 import Marquee from '../components/Marquee'
 import Header from '../components/Header/'
-import * as nearAPI from 'near-api-js';
-import { ConnectConfig } from 'near-api-js';
 
 
 function Dashboard(props){
@@ -15,27 +13,6 @@ function Dashboard(props){
   )
 }
 
-Dashboard.getInitialProps = async () => {
-  const { keyStores, connect, WalletConnection } = nearAPI;
-  const myKeyStore = new keyStores.BrowserLocalStorageKeyStore();
-  const connectionConfig: ConnectConfig = {
-      networkId: "testnet",
-      keyStore: myKeyStore,
-      nodeUrl: "https://rpc.testnet.near.org",
-      walletUrl: "https://wallet.testnet.near.org",
-      helperUrl: "https://helper.testnet.near.org",
-      headers: {}
-};
 
-  await connect(connectionConfig)
-    .then(nearConnection => {
-      const walletConnection = new WalletConnection(nearConnection, null);
-      return { walletConnection: 'hey' };
-    })
-    .catch(error => {
-      return { walletConnection: 'hey'}
-    })
-
-}
 
 export default WithFirebaseAuth(Dashboard);
