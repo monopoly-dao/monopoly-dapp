85722d4b6d841bde607ff81579e460a38a22b671
Mubaraq Mustapha <ayomidemusty@gmail.com>
2024-06-16 00:16:26 +0100

    fix: sign up

--- dapp/src/app/(grouped-layout)/profile/_utils/updateProfileValidations.ts
+++ dapp/src/app/(grouped-layout)/profile/_utils/updateProfileValidations.ts
@@ -5,6 +5,8 @@ import { UpdateProfileIds } from './updateProfileConstants';
 export const updateProfileSchema = object({
   [UpdateProfileIds.FirstName]: string().required('First name is required'),
   [UpdateProfileIds.LastName]: string().required('Last name is required'),
-  [UpdateProfileIds.Username]: string().required('Username is required'),
+  [UpdateProfileIds.Username]: string()
+    .required('Please provide your username')
+    .min(6, 'Username must have a minimum of 6 characters'),
   [UpdateProfileIds.Phone]: string().required('Phone is required'),
 });
